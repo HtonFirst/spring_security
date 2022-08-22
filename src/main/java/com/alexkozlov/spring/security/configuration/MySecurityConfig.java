ec5d056f41974bca6dec39bfb415de8f1afd32ea
HtonFirst <al.kozlov.rus@gmail.com>
2022-08-22 19:12:02 +0300

    22.08.22 first commit spring_security

---
+++ src/main/java/com/alexkozlov/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,18 @@
+package com.alexkozlov.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("Alex").password("Alex").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("Helen").password("Helen").roles("HR"))
+                .withUser(userBuilder.username("Edward").password("Edward").roles("MANAGER", "HR"));
+    }
+}
---
+++ target/spring_security/WEB-INF/view/view_for_all_employees.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h3>Information for all employees</h3>
+
+<br><br>
+
+<input type="button" value="Salary"
+       onclick="window.location.href = 'hr_info'">
+Only for HR stuff!
+<br><br>
+<input type ="button" value="Performans"
+       onclick="window.location.href = 'manager_info'">
+Only for managers!
+<br><br>
+
+</body>
+</html>
